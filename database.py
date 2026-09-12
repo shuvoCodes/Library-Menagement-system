dd47262fcf0c78f7b8c5886ee382a79286b409e6
Shakhawat Hossain Shuvo <65659322+shuvoCodes@users.noreply.github.com>
2026-09-12 12:33:27 +0600

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./library.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.dqtesnnigaztzjpzbmjw:Admin2026Shuvo@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread' : False})
 SessionLocal = sessionmaker(autoflush= False, autocommit= False, bind= engine)
 
