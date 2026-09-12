c5c82059a180250034d9affdccea0b682a0b2dbc
Shakhawat Hossain Shuvo <65659322+shuvoCodes@users.noreply.github.com>
2026-09-12 13:30:36 +0600

    Implement CORS middleware for FastAPI
    Added CORS middleware to allow cross-origin requests.

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from router import auth,admin
 from router.auth import get_current_user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind = engine)
 app.include_router(auth.route)
